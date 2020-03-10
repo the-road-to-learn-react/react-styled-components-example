18cb0ba8fdbb59d06d20c4830dc27e688ed152c8
Robin Wieruch <wrobin@gmx.net>
2020-03-10 12:21:59 +0100

    function components

--- src/App.js
+++ src/App.js
@@ -1,65 +1,53 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const ARTICLES = [
-  { id: '0', title: 'How to fetch data in React', url: 'https://www.robinwieruch.de/react-fetching-data/' },
-  { id: '1', title: 'React\'s Ecosystem as a flexible Framework', url: 'https://www.robinwieruch.de/essential-react-libraries-framework/' },
-  { id: '2', title: 'What\'s new in React 16?', url: 'https://www.robinwieruch.de/what-is-new-in-react-16/' },
-  { id: '3', title: '8 things to learn in React before using Redux', url: 'https://www.robinwieruch.de/learn-react-before-using-redux/' },
-  { id: '4', title: 'Accept Stripe Payments with React and Express', url: 'https://www.robinwieruch.de/react-express-stripe-payment/' },
-  { id: '5', title: 'Tips to learn React + Redux', url: 'https://www.robinwieruch.de/tips-to-learn-react-redux/' },
-  { id: '6', title: '10 Reasons why I moved from Angular to React', url: 'https://www.robinwieruch.de/reasons-why-i-moved-from-angular-to-react/' },
-  { id: '7', title: 'All the Conditional Renderings in React', url: 'https://www.robinwieruch.de/conditional-rendering-react/' },
-  { id: '8', title: 'Redux or MobX: An attempt to dissolve the Confusion', url: 'https://www.robinwieruch.de/redux-mobx-confusion/' },
-  { id: '9', title: 'Tips to learn React + Redux', url: 'https://www.robinwieruch.de/tips-to-learn-react-redux/' },
-  { id: '10', title: 'A gentle Introduction to React\'s Higher Order Components', url: 'https://www.robinwieruch.de/gentle-introduction-higher-order-components/' },
-  { id: '11', title: 'Complete Course to learn Redux and MobX', url: 'https://roadtoreact.com/' },
+const COURSES = [
+  {
+    id: "0",
+    title: "Road to React",
+    url: "http://roadtoreact.com/"
+  },
+  {
+    id: "1",
+    title: "Road to Firebase",
+    url: "http://roadtofirebase.com/"
+  },
+  {
+    id: "2",
+    title: "Road to GraphQL",
+    url: "http://roadtographql.com/"
+  },
+  {
+    id: "3",
+    title: "Road to Redux",
+    url: "http://roadtoredux.com/"
+  }
 ];
 
 const applyFilter = searchTerm => article =>
   article.title.toLowerCase().includes(searchTerm.toLowerCase());
 
-const applySearchTerm = searchTerm => () => ({
-  searchTerm,
-});
-
 const AppWrapper = styled.div`
   margin: 20px;
 `;
 
-class App extends React.Component<AppProps, AppState> {
-  constructor() {
-    super();
+const App = () => {
+  const [searchTerm, setSearchTerm] = React.useState("");
 
-    this.state = {
-      searchTerm: '',
-    };
+  const handleSearch = event => {
+    setSearchTerm(event.target.value);
+  };
 
-    this.onSearch = this.onSearch.bind(this);
-  }
+  return (
+    <AppWrapper>
+      <Search value={searchTerm} onSearch={handleSearch}>
+        <p>Search</p>
+      </Search>
 
-  onSearch(event) {
-    const { value } = event.target;
-
-    this.setState(applySearchTerm(value));
-  }
-
-  render() {
-    const { searchTerm } = this.state;
-
-    return (
-      <AppWrapper>
-        <Search value={searchTerm} onSearch={this.onSearch}>
-          <p>Search</p>
-        </Search>
-
-        <Articles articles={ARTICLES.filter(applyFilter(searchTerm))} />
-
-        <p>Found in <a href="https://roadtoreact.com/">the Road to learn React</a></p>
-      </AppWrapper>
-    );
-  }
-}
+      <Courses courses={COURSES.filter(applyFilter(searchTerm))} />
+    </AppWrapper>
+  );
+};
 
 const SearchWrapper = styled.div`
   display: flex;
@@ -70,14 +58,12 @@ const MyInput = styled.input`
   padding: 10px;
 `;
 
-const Search = ({ value, onSearch, children }) =>
+const Search = ({ value, onSearch, children }) => (
   <SearchWrapper>
-    {children}&nbsp;<MyInput
-      value={value}
-      onChange={onSearch}
-      type="text"
-    />
+    {children}&nbsp;
+    <MyInput value={value} onChange={onSearch} type="text" />
   </SearchWrapper>
+);
 
 const List = styled.ul`
   margin: 0;
@@ -90,20 +76,22 @@ const ListItem = styled.li`
   border-bottom: 1px solid grey;
 `;
 
-const Articles = ({ articles }) =>
+const Courses = ({ courses }) => (
   <List>
-    {articles.map(article =>
+    {courses.map(article => (
       <ListItem key={article.id}>
         <Article article={article} />
       </ListItem>
-    )}
+    ))}
   </List>
+);
 
 const MyLink = styled.a`
   text-decoration: none;
 `;
 
-const Article = ({ article }) =>
+const Article = ({ article }) => (
   <MyLink href={article.url}>{article.title}</MyLink>
+);
 
 export default App;
